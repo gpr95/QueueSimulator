5a57cc167d687ed9cc4c2f8291bac9447f967b48
Grzegorz Przytula <Grzegorz.Przytula@ipfdigital.com>
2019-04-21 20:55:50 +0200

    Create good fundamentals to queue simulator

--- src/main/groovy/mm1/simulator/Simulator.groovy
+++ src/main/groovy/mm1/simulator/Simulator.groovy
@@ -5,57 +5,161 @@ class Simulator {
     Double mu
     Integer probes
     Integer seed
+    Integer eventsInSystem
+    Integer eventsInQueue
+
+    Double timeInQueue
+    Double timeInSystem
+
+
     Queue queue
+    System system
+
     Simulator(Double lambda, Double mu, Integer probes, Integer seed) {
         this.lambda = lambda
         this.mu = mu
         this.probes = probes
         this.seed = seed
-        this.queue = new Queue()
+        this.system = new System(mu, seed)
+        this.queue = new Queue(lambda, seed)
+        eventsInSystem = 0
+        eventsInQueue = 1
+        timeInSystem = 0
+        timeInQueue = 0
     }
 
     void simulate() {
-        Poisson timeToDie = new Poisson(seed, lambda)
+        // Single event is generated in queue constructor
+        Queue queue = new Queue(lambda, seed)
+        System system = new System(mu, seed)
+
         0.upto(probes, {
-            this.queue.addEvent(timeToDie.getRandomArrival(), EventType.ARRIVAL)
-            this.queue.consumeEvent(new Poisson(seed, lambda).getRandomArrival())
+            // Single event is removed from queue in consumer,
+            Event event = queue.consumeEvent()
+
+            //
+
+            println(event.type)
+            println(event.timeToStart)
+            consume(event, system, queue)
+
+            //
+
+            Event systemEvent = system.consumeEvent()
+
+            //
+
+            println(event.type)
+            println(event.timeToStart)
+            consume(event, system, queue)
+
+            //
+
+            queue.addEvent(queue.generateEvent())
         })
+    }
+
+    void consume(Event event, System system, Queue queue) {
+        if(!event?.timeToStart)
+            return
+        system.decreaseTimeRemainingToRemoveFromList(Math.abs(event.timeToStart - system.previousEventTime))
+        queue.decreaseTimeRemainingToRemoveFromList(Math.abs(event.timeToStart - queue.previousEventTime))
+
+        switch(event.type) {
+            case EventType.QUEUE:
+                this.eventsInQueue--
+                this.eventsInSystem++
+
+                Event eventToSystem = system.generateEvent()
+                this.timeInSystem += eventToSystem.timeToStart
+                system.addEvent(eventToSystem)
+                break
+            case EventType.SYSTEM:
+                this.eventsInSystem--
+                break
+        }
     }
 
 }
 
 
-class Queue {
+
+class EventConsumer {
     List<Event> eventList
     Double previousEventTime
+    Integer seed
+    Double poissonParameter
+    Double timeRemainingToRemoveFromList
+    Random generator
 
-    Queue(Double initTime) {
-        eventList = new LinkedList<>()
-        addEvent(initTime, EventType.ARRIVAL);
+    EventConsumer(Integer seed) {
+        generator = new Random(seed)
     }
 
-    void addEvent(Double time, EventType type) {
-        eventList.add(new Event(time, type))
+    void addEvent(Event event) {
+        boolean success = false
+
+        for(Event oldEvent : eventList) {
+            if(oldEvent > event) {
+                eventList.add(eventList.indexOf(oldEvent), event)
+                success = true
+                break
+            }
+        }
+
+        // Add to the end if time to start is the longest
+        if(!success)
+            eventList.add(event)
     }
 
-    Event consumeEvent(Double nextEventArrivalTime) {
+    Event generateEvent() {
+        def randomTime =  Math.log(1.0-generator.nextDouble())/-poissonParameter
+        return new Event(randomTime, EventType.QUEUE)
+    }
+
+    Event consumeEvent() {
         if(!eventList.isEmpty()) {
             Event event = eventList.first()
-            process(event)
             previousEventTime = event.timeToStart
             eventList.remove(0)
-            addEvent(nextEventArrivalTime, EventType.ARRIVAL)
             return event
         }
         else return null
     }
 
-    void process(Event event) {
-
-    }
-
     Boolean isEmpty() {
         return eventList.isEmpty()
+    }
+
+    void decreaseTimeRemainingToRemoveFromList(Double time) {
+        this.timeRemainingToRemoveFromList -= time
+        if(this.timeRemainingToRemoveFromList < 0)
+            this.timeRemainingToRemoveFromList = 0
+    }
+}
+
+
+class Queue extends EventConsumer{
+    Queue(Double lambda, Integer seed) {
+        super(seed)
+        this.eventList = new LinkedList<>()
+        this.poissonParameter = lambda
+        this.seed = seed
+        this.previousEventTime = 0
+        this.timeRemainingToRemoveFromList = 0
+
+        addEvent(generateEvent())
+    }
+}
+
+class System extends EventConsumer{
+    System(Double mu, Integer seed) {
+        super(seed)
+        this.eventList = new LinkedList<>()
+        this.poissonParameter = mu
+        this.seed = seed
+        this.previousEventTime = 0
+        this.timeRemainingToRemoveFromList = 0
     }
 }
 
@@ -84,17 +188,7 @@ class Event implements Comparable<Event>{
     }
 }
 
-class Result {
-    Integer numberOfArrivals = 0;
-    Integer numberOfDepartures = 0;
-    Double averageNumberInSystem = 0;
-    Double simulationTime = 0;
-    Double averageTimeToService = 0;
-    Double averageEventTime = 0;
-    Double averageNumberOfWaitingTasks = 0;
-}
-
 enum EventType {
-    ARRIVAL, DEPARTURE
+    QUEUE, SYSTEM
 }
 
